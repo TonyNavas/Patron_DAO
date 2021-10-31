3717e05fe670b2d8119981480782b87bbf00283b
TonyNavas <tonynavas777@gmail.com>
2021-10-31 13:46:16 -0600

    Se crean los metodos

--- src/main/java/org/geronimo/Decorator.java
+++ src/main/java/org/geronimo/Decorator.java
@@ -1,2 +1,34 @@
-package org.geronimo;public class Decorator {
+package org.geronimo;
+
+public abstract class Decorator implements Cuenta{
+    private Cuenta cuenta;
+    public Decorador(Cuenta cuenta) {
+        this.cuenta = cuenta;
+    }
+    public void setCuenta(Cuenta cuenta) {
+        this.cuenta = cuenta;
+    }
+    public Cuenta getCuenta() {
+        return cuenta;
+    }
+    public double saldo() {
+        return cuenta.saldo();
+    }
+    public void ingresar(double cantidad) {
+        cuenta.ingresar(cantidad);
+    }
+    public void retirar(double cantidad) {
+        cuenta.retirar(cantidad);
+    }
+    public Cliente getCliente() {
+        return cuenta.getCliente();
+    }
+    public Fecha getFecApe() {
+        return cuenta.getFecApe();
+    }
+    @Override
+    public String toString() {
+        return cuenta.toString();
+    }
+    public abstract void aplicaIntereses();
 }
