7b3b39c82936e9348b4463f163eb2f1b4431c028
TonyNavas <tonynavas777@gmail.com>
2021-10-31 13:59:09 -0600

    Se crea la clase cuentaPlazo

--- src/main/java/org/geronimo/CuentaPlazo.java
+++ src/main/java/org/geronimo/CuentaPlazo.java
@@ -1,2 +1,27 @@
-package org.geronimo;public class CuentaPlazo {
+package org.geronimo;
+
+public class CuentaPlazo extends Decorator {
+    //interés aplicado a la cuenta a plazo
+    private double interes;
+    //plazo en años
+    private int plazo;
+    public CuentaPlazo(Cuenta c, double interes, int plazo) {
+        super(c);
+        this.interes = interes;
+        this.plazo = plazo;
+    }
+    public void aplicaIntereses() {
+        double saldoActual = super.saldo();
+        super.getCuenta().ingresar(saldoActual * (interes / 100) * plazo);
+    }
+    public double getInteres() {
+        return this.interes;
+    }
+    public int getPlazo() {
+        return this.plazo;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + "\nInterés: " + interes + " - Plazo: " + plazo;
+    }
 }
