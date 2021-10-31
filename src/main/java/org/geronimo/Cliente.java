705f6ab05752304e1d0938cc8c129cd522be4b27
TonyNavas <tonynavas777@gmail.com>
2021-10-31 13:42:25 -0600

    Se crean los metodos para los atributos

--- src/main/java/org/geronimo/Cliente.java
+++ src/main/java/org/geronimo/Cliente.java
@@ -6,7 +6,7 @@ public class Cliente {
     private Fecha fechaNac;
     private String direccion;
     private String tfno;
-    
+
     public Cliente(String p_dni, String p_nombre, Fecha p_fecnac, String p_direccion,
                    String p_tfno) {
         dni = p_dni;
@@ -14,5 +14,44 @@ public class Cliente {
         fechaNac = p_fecnac;
         direccion = p_direccion;
         tfno = p_tfno;
+    }
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Fecha getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(Fecha fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTfno() {
+        return tfno;
+    }
+
+    public void setTfno(String tfno) {
+        this.tfno = tfno;
     }
 }
