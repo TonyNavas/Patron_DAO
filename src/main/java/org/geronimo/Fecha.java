adbf0083ba3575ee23ea1bddd7c041a84a35606a
TonyNavas <tonynavas777@gmail.com>
2021-10-31 13:48:05 -0600

    Se crean los atributos y el constructor

--- src/main/java/org/geronimo/Fecha.java
+++ src/main/java/org/geronimo/Fecha.java
@@ -1,4 +1,13 @@
 package org.geronimo;
 
 public class Fecha {
+    private int año;
+    private int mes;
+    private int dia;
+
+    public Fecha(int dia, int mes, int año) {
+        this.dia = dia;
+        this.mes = mes;
+        this.año = año;
+    }
 }
